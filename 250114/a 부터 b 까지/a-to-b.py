34fd727b07e6bb61efc90e725089e09857cd6b35
Akisigure <122990146+Akisigure@users.noreply.github.com>
2025-01-14 10:35:52 +0900

    Title: a 부터 b 까지, Time: 42ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250114/a 부터 b 까지/a-to-b.py
@@ -0,0 +1,11 @@
+a,b = map(int,input().split())
+
+for _ in range(a,b):
+    if a > b:
+        break
+    if a % 2 == 0:
+        print(a, end=' ')
+        a += 3
+    else :
+        print(a, end=' ')
+        a *= 2
