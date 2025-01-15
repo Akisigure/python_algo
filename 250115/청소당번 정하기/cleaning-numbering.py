8ff9868eb2cdfbe8234f0407b667874641b0e48f
Akisigure <122990146+Akisigure@users.noreply.github.com>
2025-01-15 16:40:52 +0900

    Title: 청소당번 정하기, Time: 43ms, Memory: 16MB, Status: Passed - CodeTree

--- 250115/청소당번 정하기/cleaning-numbering.py
+++ 250115/청소당번 정하기/cleaning-numbering.py
@@ -7,8 +7,10 @@ c = 0
 for i in range(1,n+1):
     if i % 12 == 0 and i % 3 == 0 and i % 2 == 0:
         c += 1
+        continue
     elif i % 3 == 0:
         b += 1
+        continue
     elif i % 2 == 0:
         a += 1
     
