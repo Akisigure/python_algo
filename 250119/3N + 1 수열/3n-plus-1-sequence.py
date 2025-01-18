42d003a97a4fe4db1757514e99118196cacab148
Akisigure <122990146+Akisigure@users.noreply.github.com>
2025-01-19 01:30:15 +0900

    Title: 3N + 1 수열, Time: 43ms, Memory: 16MB, Status: Passed - CodeTree

--- 250119/3N + 1 수열/3n-plus-1-sequence.py
+++ 250119/3N + 1 수열/3n-plus-1-sequence.py
@@ -2,14 +2,15 @@ cnt = 0
 a = int(input())
 while True:
 
-    if a % 2 == 0:
+    if a == 1:
+        break
+    elif a % 2 == 0:
         a = a // 2
         cnt += 1
     elif a % 2 != 0:
         a = a * 3 + 1
         cnt += 1
-    if a == 1:
-        break
+   
 
 print(cnt)
 
