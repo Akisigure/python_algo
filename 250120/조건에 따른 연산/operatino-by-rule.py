c0701b11c044a10772964c68de831d3ef6a1c303
Akisigure <122990146+Akisigure@users.noreply.github.com>
2025-01-20 09:22:36 +0900

    Title: 조건에 따른 연산, Time: 42ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250120/조건에 따른 연산/operatino-by-rule.py
@@ -0,0 +1,15 @@
+a = int(input())
+cnt = 0
+
+while True:
+    if a >= 1000 :
+        break
+    if a % 2 == 0 :
+        a = a * 3 + 1
+        cnt += 1
+    elif a % 2 != 0:
+        a = a * 2 + 2
+        cnt += 1
+
+print(cnt)
+    
