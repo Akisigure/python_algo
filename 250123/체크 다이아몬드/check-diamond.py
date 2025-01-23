70e327131b912a93bca417a498781bbf33f736d7
Akisigure <122990146+Akisigure@users.noreply.github.com>
2025-01-23 13:49:22 +0900

    Title: 체크 다이아몬드, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250123/체크 다이아몬드/check-diamond.py
@@ -0,0 +1,19 @@
+n = int(input())
+
+for i in range(n):
+
+    for j in range(i+1,n):
+        print(' ',end='')
+
+    for j in range(i+1):
+        print('*', end=' ')
+
+    print()
+
+for i in range(n):
+    
+    for j in range(i + 1):
+        print(' ',end='')
+    for j in range(i+1,n):
+        print('*',end=' ')
+    print()
