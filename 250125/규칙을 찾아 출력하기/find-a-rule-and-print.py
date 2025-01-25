8c98d20e50cb739038be03b659a7ae69d5b7c6ae
Akisigure <122990146+Akisigure@users.noreply.github.com>
2025-01-25 17:56:51 +0900

    Title: 규칙을 찾아 출력하기, Time: 42ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250125/규칙을 찾아 출력하기/find-a-rule-and-print.py
@@ -0,0 +1,14 @@
+n = int(input())
+
+a = n
+
+for i in range(n):
+    for j in range(n):
+        if n == 1:
+            print('*', end=' ')
+        else :
+            if (i and j) !=0 and (i and j)!=n-1 and i <= j:
+                print(' ',end=' ')
+            else :
+                print('*',end=' ')
+    print()
