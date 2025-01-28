9cc3bd979372b02fc48a01fc77be11616a403d1e
Akisigure <122990146+Akisigure@users.noreply.github.com>
2025-01-28 22:24:59 +0900

    Title: 배열의 특정 위치 찾기 3, Time: 43ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250128/배열의 특정 위치 찾기 3/find-specific-location-fo-array-3.py
@@ -0,0 +1,12 @@
+arr = list(map(int,input().split()))
+
+arr2 = []
+for i in arr :
+    if i == 0:
+        break
+    else :
+        arr2.append(i)
+
+s = arr2[-1] + arr2[-2] + arr2[-3]
+
+print(s)
