20803cc8c89ddd0cb5758c6c01e9751f83f015b0
Akisigure <122990146+Akisigure@users.noreply.github.com>
2025-01-29 15:48:33 +0900

    Title: 십의 자리 숫자, Time: 41ms, Memory: 16MB, Status: Passed - CodeTree

--- 250129/십의 자리 숫자/number-of-tens-digit.py
+++ 250129/십의 자리 숫자/number-of-tens-digit.py
@@ -2,7 +2,11 @@ c_arr = [0] * 10
 arr = list(map(int,input().split()))
 
 for i in arr :
-    c_arr[i // 10] += 1
+    if i == 0:
+        c_arr[i // 10] += 1
+        break
+    else :
+        c_arr[i // 10] += 1
 
 c_len = len(c_arr)
 
