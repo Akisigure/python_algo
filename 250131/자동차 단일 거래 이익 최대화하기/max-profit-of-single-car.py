fbbad7e5aa2138a0182022f77e2c1161e5d61e54
Akisigure <122990146+Akisigure@users.noreply.github.com>
2025-01-31 23:51:53 +0900

    Title: 자동차 단일 거래 이익 최대화하기, Time: 54ms, Memory: 17MB, Status: Passed - CodeTree

--- 250131/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.py
+++ 250131/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.py
@@ -1,27 +1,10 @@
-
-import sys
-
 n = int(input())
 price = list(map(int, input().split()))
-
-min_price = sys.maxsize
-max_price = -sys.maxsize
-min_idx = 0
 calc = 0
-# Write your code here!
 
-for i in range(n):
-    if price[i] < min_price :
-        min_price = price[i]
-        min_idx = i
-
-for i in price[min_idx:] :
-    if i > max_price :
-        max_price = i
-
-if max_price < min_price :
-    calc = 0
-
-calc = max_price - min_price
+for i in range(n - 1):
+    for j in range(i, n):
+        if price[i] < price[j] and price[j] - price[i] > calc:
+            calc = price[j] - price[i]
 
 print(calc)
