a80630a51aa9b7002c5bea0833e99b0196707b6d
Akisigure <122990146+Akisigure@users.noreply.github.com>
2025-01-31 10:32:50 +0900

    Title: 500 근처의 수, Time: 42ms, Memory: 16MB, Status: Passed - CodeTree

--- 250131/500 근처의 수/near-500.py
+++ 250131/500 근처의 수/near-500.py
@@ -2,20 +2,24 @@ import sys
 
 r_num = 500
 max_value = sys.maxsize
-min_value = sys.maxsize
-temp = []
-
+min_value = -sys.maxsize
+max_temp = []
+min_temp = []
 arr = list(map(int,input().split()))
 
 for i in arr :
     if i > r_num :
-        temp.append(i)
+        max_temp.append(i)
     if i < r_num :
-        min_value = i
+        min_temp.append(i)
 
-for i in temp :
+for i in max_temp :
     if i < max_value :
         max_value = i
+
+for i in min_temp :
+    if i > min_value :
+        min_value = i
         
 
 print(min_value,max_value)     
