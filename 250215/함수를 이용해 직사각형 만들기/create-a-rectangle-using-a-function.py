1824b0da2ed4f450a30f70598523c64d418d1952
Akisigure <122990146+Akisigure@users.noreply.github.com>
2025-02-15 22:41:49 +0900

    Title: 함수를 이용해 직사각형 만들기, Time: 60ms, Memory: 17MB, Status: Passed - CodeTree

---
+++ 250215/함수를 이용해 직사각형 만들기/create-a-rectangle-using-a-function.py
@@ -0,0 +1,11 @@
+n, m = map(int, input().split())
+
+# Write your code here!
+
+def ractangle(arg1, arg2):
+    for i in range(arg1):
+        for j in range(arg2):
+            print(1,end='')
+        print()
+
+ractangle(n,m)
