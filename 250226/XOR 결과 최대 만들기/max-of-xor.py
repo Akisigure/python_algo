03ca6fd5c461cd2944fba829c0ea9f34a19e83a0
Akisigure <122990146+Akisigure@users.noreply.github.com>
2025-02-26 00:34:25 +0900

    Title: XOR 결과 최대 만들기, Time: 86ms, Memory: 22MB, Status: Passed - CodeTree

--- 250226/XOR 결과 최대 만들기/max-of-xor.py
+++ 250226/XOR 결과 최대 만들기/max-of-xor.py
@@ -1,16 +1,14 @@
 from itertools import combinations
+from functools import reduce
 
 n, m = map(int, input().split())
 A = list(map(int, input().split()))
 
-lst = list(combinations(A,m))
-mv = -1
-# Write your code here!
+lst = list(combinations(A, m))
+mv = -1 
 
 for i in lst:
-    xor_value = i[0]
-    for num in i[1:]:
-        xor_value ^= num
-    mv = max(mv,xor_value)
+    xor_value = reduce(lambda x, y: x ^ y, i) 
+    mv = max(mv, xor_value)
 
 print(mv)
