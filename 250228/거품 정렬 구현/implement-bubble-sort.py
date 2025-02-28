22af35d89a0f05943c6448fd79e447ea02f5fc0e
Akisigure <122990146+Akisigure@users.noreply.github.com>
2025-02-28 09:02:30 +0900

    Title: 거품 정렬 구현, Time: 46ms, Memory: 16MB, Status: Passed - CodeTree

--- 250228/거품 정렬 구현/implement-bubble-sort.py
+++ 250228/거품 정렬 구현/implement-bubble-sort.py
@@ -4,7 +4,7 @@ arr = list(map(int, input().split()))
 # Please write your code here.
 
 for i in range(n):
-    for j in range(i,n-1):
+    for j in range(i,n):
         if arr[i] > arr[j]:
             arr[i],arr[j] = arr[j],arr[i]
 
