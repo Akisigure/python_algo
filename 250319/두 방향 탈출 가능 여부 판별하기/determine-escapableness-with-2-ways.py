1c798fb508735a55c5ab3ac925e7f6c212cbbf6d
Akisigure <122990146+Akisigure@users.noreply.github.com>
2025-03-19 01:28:09 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 63ms, Memory: 19MB, Status: Passed - CodeTree

--- 250319/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
+++ 250319/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
@@ -1,4 +1,4 @@
-dxy = [[0,1],[1,0]]
+dxy = [[1,0],[0,1]]
 
 n,m = map(int,input().split())
 
@@ -7,7 +7,9 @@ arr = [list(map(int,input().split())) for _ in range(n)]
 visited = [[False] * m for _ in range(n)]
 
 res = 0
-def bfs(x,y,):
+
+
+def bfs(x,y):
 
     global res
 
@@ -24,10 +26,9 @@ def bfs(x,y,):
         nx = dx + x
         ny = dy + y
 
-        if 0 <= nx < n and 0 <= ny < n and arr[nx][ny] == 1 and not visited[nx][ny]:
-            bfs(nx,ny)
+        if 0 <= nx < n and 0 <= ny < m   and arr[nx][ny] == 1 and not visited[nx][ny]:
+            bfs(nx, ny)
 
 
 bfs(0,0)
-
 print(res)
