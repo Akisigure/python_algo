2154c89b9ef8c5582431cf59801c92ba7d3d1782
Akisigure <122990146+Akisigure@users.noreply.github.com>
2025-04-02 17:51:11 +0900

    Title: 연결된 정점, Time: 5065ms, Memory: 24MB, Status: Time Limit Exceed - Codetree

---
+++ 250402/연결된 정점/connected-vertex.py
@@ -0,0 +1,45 @@
+n, m = map(int,input().split())
+
+node = [0] * (n + 1)
+
+
+def make_set(x):
+    node[x] = x
+
+
+def find_set(x):
+    if node[x] != x:
+        node[x] = find_set(node[x])
+    return node[x]
+
+
+def union(x,y):
+    px = find_set(x)
+    py = find_set(y)
+
+    if node[px] > node[py]:
+        node[px] = py
+    if node[px] < node[py]:
+        node[py] = px
+
+
+for k in range(1, n + 1):
+    make_set(k)
+
+
+for i in range(m):
+    cnt = 0
+    lst = list(input().split())
+
+    if lst[0] == 'x':
+        a, b = int(lst[1]),int(lst[2])
+        union(a,b)
+    if lst[0] == 'y':
+        val = int(lst[1])
+        for v in range(len(node)):
+            find_set(v)
+        temp = []
+        for j in range(len(node)):
+            if node[j] == val:
+                cnt += 1
+        print(cnt)
